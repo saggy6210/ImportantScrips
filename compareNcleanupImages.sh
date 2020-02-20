a2c39f6546b77d1e83cce9094321348a18f910a5
Sagar Chavan <sagarchavan6210@gmail.com>
2020-02-20 12:52:57 +0530

    updated with container stop commands and echo statement

--- compareNcleanupImages.sh
+++ compareNcleanupImages.sh
@@ -4,24 +4,25 @@
 container_list=`cat compose.yaml | grep image | awk '{print $2}' | awk -F '/' '{print $2}' |awk -F':' '{print $1}'`
 services=`docker images | awk '{print $1}' | awk -F '/' '{print $2}'`
 a=()
-    for i in $container_list; do
-        found=
+for i in $container_list; do
+	found=
         for j in $services; do
-            if [ $i == $j ]; then
-                found=1
-                echo "$j"
-                break
-            fi
+            	if [ $i == $j ]; then
+                	found=1
+                	echo "Service Name exist in  compose file: $j"
+                	break
+            	fi
         done
         if [ !$found ]; then
+	    echo "Service Name does exist in  compose file, Stoping container service: $i"
+	    docker stop $i
             a+=($i)
         fi
-    done
+done
 
-    images_list=`echo ${a[*]}`
-    for image in $images_list
-    do
-            image_id=`docker images | grep $image | awk '{print $3}'`
-            echo "removing docker image :$image" 
-			docker rmi -f $image_id
-    done
+images_list=`echo ${a[*]}`
+for image in $images_list; do
+	image_id=`docker images | grep $image | awk '{print $3}'`
+	echo "removing docker image :$image" 
+	docker rmi -f $image_id
+done
