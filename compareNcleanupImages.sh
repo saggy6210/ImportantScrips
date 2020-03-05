ca7e693ef1a5d18f94b0e6d8e92433aca683c10e
Sagar Chavan <sagarchavan6210@gmail.com>
2020-03-05 17:34:39 +0530

    Update compareNcleanupImages.sh

--- compareNcleanupImages.sh
+++ compareNcleanupImages.sh
@@ -1,28 +1,45 @@
 #!/bin/bash
 #________Author: Sagar Chavan______________
-
-container_list=`cat compose.yaml | grep image | awk '{print $2}' | awk -F '/' '{print $2}' |awk -F':' '{print $1}'`
-services=`docker images | awk '{print $1}' | awk -F '/' '{print $2}'`
+running_container_list=`docker container ls | awk 'FNR > 1 {print $2}' | awk -F '/' '{print $2}'`
+service_list=`cat compose.yaml_new | grep image | awk '{print $2}' | awk -F '/' '{print $2}'`
+#services=`docker images | awk '{print $1}' | awk -F '/' '{print $2}'`
 a=()
-for i in $container_list; do
+for i in $running_container_list; do
 	found=
-        for j in $services; do
+        for j in $service_list; do
             	if [ $i == $j ]; then
+			#echo " $i || $j"
                 	found=1
-                	echo "Service Name exist in  compose file: $j"
+			echo "--------------------------------------------------------------"
+			echo 
+                	echo "Container Name exist in compose file: $j"
+                        echo "--------------------------------------------------------------"
+                        echo 
                 	break
             	fi
         done
-        if [ !$found ]; then
-	    echo "Service Name does exist in  compose file, Stoping container service: $i"
-	    docker stop $i
-            a+=($i)
+        if [ "$found" != "1" ]; then
+		echo "--------------------------------------------------------------"
+            	echo 
+	    	echo "Container does not exist in compose file: $i"
+	    	container_id=`docker container ls | grep $i | awk '{print $1}'`
+	    	image_name=`echo $i | awk -F':' '{print $1}'`
+	    	image_id=`docker images | grep $image_name | awk '{print $3}'`
+	    	echo "Stopping the container $container_id"
+            	docker stop $container_id
+	    	echo "Removing docker Image Name: $image_name, Image ID: $image_id"
+	    	docker rmi -f $image_id
+            	#a+=($i)
+            	echo "--------------------------------------------------------------"
+            	echo 
         fi
 done
+#images_list=`echo ${a[*]}`
+#for image in $images_list; do
+#	image_id=`docker images | grep $image | awk '{print $3}'`
+#	echo "removing docker image :$image" 
+#	#docker rmi -f $image_id
+#done
+echo "Cleanup Job Completed"
 
-images_list=`echo ${a[*]}`
-for image in $images_list; do
-	image_id=`docker images | grep $image | awk '{print $3}'`
-	echo "removing docker image :$image" 
-	docker rmi -f $image_id
-done
+
