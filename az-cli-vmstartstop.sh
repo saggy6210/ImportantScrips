aaaaa90b1771158b8213ed65c1296f3efa9735b2
Sagar Chavan <sagarchavan6210@gmail.com>
2020-08-22 16:48:18 +0530

    Update az-cli-vmstartstop.sh

--- az-cli-vmstartstop.sh
+++ az-cli-vmstartstop.sh
@@ -4,9 +4,9 @@
 action=$1
 az login --service-principal -u <CLIENT_ID> -p <CLIENT_SECRET> --tenant <TENANT_ID>
 az account set --subscription <SUB_ID>
-list=$(az vm list -g ch-sanity-1 --query [].id --output tsv | awk -F "/" '{print $NF}' | sed 's/"//g')
+list=$(az vm list -g <resource_group_name> --query [].id --output tsv | awk -F "/" '{print $NF}' | sed 's/"//g')
 for vmname in $list
 do
     echo ${vmname}
-    az vm ${action} --resource-group ch-sanity-1 --name ${vmname}
+    az vm ${action} --resource-group <resource_group_name> --name ${vmname}
 done
